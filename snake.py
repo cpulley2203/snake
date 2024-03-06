e2a04b5e4790a66ae5fd8b79690c5de86fe1c1e7
cpulley2203 <79099797+cpulley2203@users.noreply.github.com>
2024-03-06 12:21:45 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,58 +1,132 @@
-import random
-import curses
+import tkinter
+import random  
 
-s = curses.initscr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
+ROWS = 25
+COLS = 25
+TILE_SIZE = 25
 
-snk_x = sw/4
-snk_y = sh/2
-snake = [
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
+WINDOW_WIDTH = TILE_SIZE * COLS #25*25 = 625
+WINDOW_HEIGHT = TILE_SIZE * ROWS #25*25 = 625
 
-food = [sh/2, sw/2]
-w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-key = curses.KEY_RIGHT
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
 
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
+canvas = tkinter.Canvas(window, bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT, borderwidth = 0, highlightthickness = 0)
+canvas.pack()
+window.update()
 
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
+#center the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 
-    new_head = [snake[0][0], snake[0][1]]
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
 
-    if key == curses.KEY_DOWN:
-        new_head[0] += 1
-    if key == curses.KEY_UP:
-        new_head[0] -= 1
-    if key == curses.KEY_LEFT:
-        new_head[1] -= 1
-    if key == curses.KEY_RIGHT:
-        new_head[1] += 1
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
-    snake.insert(0, new_head)
+#initialize game
+snake = Tile(TILE_SIZE * 5, TILE_SIZE * 5) #single tile, snake's head
+food = Tile(TILE_SIZE * 10, TILE_SIZE * 10)
+velocityX = 0
+velocityY = 0
+snake_body = [] #multiple snake tiles
+game_over = False
+score = 0
 
-    if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
+#game loop
+def change_direction(e): #e = event
+    # print(e)
+    # print(e.keysym)
+
+    global velocityX, velocityY, game_over
+    if (game_over):
+        return #edit this code to reset game variables to play again
+
+    if (e.keysym == "Up" and velocityY != 1):
+        velocityX = 0
+        velocityY = -1
+        
+    elif (e.keysym == "Down" and velocityY != -1):
+        velocityX = 0
+        velocityY = 1
+
+    elif (e.keysym == "Left" and velocityX != 1):
+        velocityX = -1
+        velocityY = 0
+
+    elif (e.keysym == "Right" and velocityX != -1):
+        velocityX = 1
+        velocityY = 0
+
+
+def move():
+    global snake, food, snake_body, game_over, score
+    if (game_over):
+        return
+    
+    if (snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y >= WINDOW_HEIGHT):
+        game_over = True
+        return
+    
+    for tile in snake_body:
+        if (snake.x == tile.x and snake.y == tile.y):
+            game_over = True
+            return
+    
+    #collision
+    if (snake.x == food.x and snake.y == food.y): 
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS-1) * TILE_SIZE
+        food.y = random.randint(0, ROWS-1) * TILE_SIZE
+        score += 1
+
+    #update snake body
+    for i in range(len(snake_body)-1, -1, -1):
+        tile = snake_body[i]
+        if (i == 0):
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i-1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
+    
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
+
+
+def draw():
+    global snake, food, snake_body, game_over, score
+    move()
+
+    canvas.delete("all")
+
+    #draw food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = 'yellow')
+
+    #draw snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = 'lime green')
+
+    for tile in snake_body:
+        canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill = 'lime green')
+
+    if (game_over):
+        canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, font = "Arial 20", text = f"Game Over. You did great! : {score}", fill = "white")
     else:
-        tail = snake.pop()
-        w.addch(int(tail[0]), int(tail[1]), ' ')
+        canvas.create_text(30, 20, font = "Arial 10", text = f"Score: {score}", fill = "white")
+    
+    window.after(100, draw) #call draw again every 100ms (1/10 of a second) = 10 frames per second
 
-    w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+draw()
+window.bind("<KeyRelease>", change_direction) #when you press on any key and then let go
+window.mainloop() #used for listening to window events like key presses
